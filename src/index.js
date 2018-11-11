2864118c648eb733dc476aa1572b47a4743ae9a1
moweng <1308490015@qq.com>
2018-11-11 16:32:17 +0800

    [modify]初始化

--- src/index.js
+++ src/index.js
@@ -1,131 +1,136 @@
-import React, { Component } from 'react';
-import { getQueryName, getSrc, resize, isArray } from './util';
-
-class ImageLoaderV2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: props.images,
-    }
-    this.registImageNode = this.registImageNode.bind(this);
-    this.getSrc = this.getSrc.bind(this);
-    this.onLoadImage = this.onLoadImage.bind(this);
-    this.onImageError = this.onImageError.bind(this);
-
-    this.images = {};
+class PictureCut {
+  constructor(type, options) {
+    this.type = type;
+    this.options = options;
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      images: nextProps.images
-    });
+  initCanvas(id, width, height) {
+    this._crop_canvas = document.createElement('canvas');
+    this._crop_canvas.width = width;
+    this._crop_canvas.height = height;
+    this.ctx = this._crop_canvas.getContext('2d');
+    // 添加在网页上调试
+    this._crop_canvas.style = 'border: 1px solid red;';
+    document.body.appendChild(this._crop_canvas);
   }
 
-  registImageNode(node, index){
-    this.images[index] = node;
+  //裁剪椭圆
+  ellipseOne(ctx, x, y, a, b) {
+    const step = a > b ? 1 / a : 1 / b;
+    ctx.beginPath();
+    ctx.moveTo(x + a, y);
+    for (let i = 0; i < 2 * Math.PI; i += step) {
+      ctx.lineTo(x + a * Math.cos(i), y + b * Math.sin(i));
+    }
+    ctx.closePath();
+    ctx.clip();
   }
 
-  getSrc(src){
-    
-    let source = getSrc({...this.props, source: src});
-    return  source;
+
+  circleOne(ctx, x, y, radius) {
+    ctx.beginPath();
+    console.log('x:', x);
+    console.log('y:', y);
+    console.log('radius:', radius);
+    ctx.arc(x, y , radius, 0, 2 * Math.P); //画出圆
+    ctx.closePath();
+    ctx.clip(); //裁剪上面的圆形
   }
 
-  onLoadImage(index){
-    if (this.props.isPreloadMode === true) {
-      return;
-    }
+  cutToEllipse() {
+    const centerX = this.options.centerX;
+    const centerY = this.options.centerY;
+    const radiusX = this.options.radiusX;
+    const radiusY = this.options.radiusY;
+    console.log('radiusX:', radiusX);
+    console.log('radiusY', radiusY);
+    const originWidth = this.options.originWidth;
+    const originHeight = this.options.originHeight;
 
-
-    const imageNode = this.images[index];
-    if(imageNode){
-      imageNode.style.opacity = 1;
-    }
+    const widthRatio = originHeight ? this.image.width / originWidth : 1;
+    const heightRatio = originHeight ? this.image.height / originHeight : 1;
+    const left = (centerX - radiusX) * widthRatio;
+    const top = (centerY - radiusY) * heightRatio;
+    const width = radiusX * 2 * widthRatio;
+    const height = radiusY * 2 * heightRatio;
+    this.ellipseOne(this.ctx, this._crop_canvas.width / 2, this._crop_canvas.height / 2, this._crop_canvas.width / 2, this._crop_canvas.height / 2);
+    return this.toCanvas(this.ctx, left, top, width, height, 0, 0, this._crop_canvas.width, this._crop_canvas.height);
   }
 
-  onImageError (index){
-    const { errorImage } = this.props;
-    console.log('error:', errorImage)
-    const imageNode = this.images[index];
-    console.log('imageNode:', imageNode);
-    if(imageNode && errorImage && !imageNode.hasLoaded){
-      imageNode.src = errorImage;
-      imageNode.hasLoaded = true;
-    }
+
+  cutToRectTangle() {
+    const originWidth = this.options.originWidth;
+    const originHeight = this.options.originHeight;
+    const widthRatio = originHeight ? this.image.width / originWidth : 1;
+    const heightRatio = originHeight ? this.image.height / originHeight : 1;
+    const left = this.options.left * widthRatio;
+    const top = this.options.top * heightRatio;
+    const width = this.options.width * widthRatio;
+    const height = this.options.height * heightRatio;
+    return this.toCanvas(this.ctx, left, top, width, height, 0, 0, this._crop_canvas.width, this._crop_canvas.height);
   }
 
-  render() {
-    const images = this.state.images;
-    let imagesArr;
-    if(!images) {
-      return '';
+  toCanvas(ctx, imageLeft, imageTop, imageWidth, imageHeight, canvasLeft = 0, canvasTop = 0, canvasWidth, canvasHeight) {
+    ctx.drawImage(
+      this.image,
+      imageLeft,
+      imageTop,
+      imageWidth,
+      imageHeight,
+      canvasLeft,
+      canvasTop,
+      canvasWidth,
+      canvasHeight,
+    );
+    const url = this._crop_canvas.toDataURL();
+
+    if (this.options.onCutDone) {
+      this.options.onCutDone(url);
     }
+    return this._crop_canvas.toDataURL();
+  }
 
-    if(!isArray(images)) {
-      imagesArr = [images];
-    }else {
-      imagesArr = images;
-    }
-    if (imagesArr.length <= 0) {
-      return '';
-    }
+  cutToCirCle() {
+    const centerX = this.options.centerX;
+    const centerY = this.options.centerY;
+    const radius = this.options.radius;
 
-    const { imageStyle } = this.props;
+    const originWidth = this.options.originWidth;
+    const originHeight = this.options.originHeight;
 
+    const widthRatio = originHeight ? this.image.width / originWidth : 1;
+    const heightRatio = originHeight ? this.image.height / originHeight : 1;
+    const left = (centerX - radius) * widthRatio;
+    const top = (centerY - radius) * heightRatio;
+    const width = radius * 2 * widthRatio;
+    const height = radius * 2 * heightRatio;
+    this.circleOne(this.ctx, this._crop_canvas.width / 2, this._crop_canvas.height / 2, this._crop_canvas.width / 2);
+    return this.toCanvas(this.ctx, left, top, width, height, 0, 0, this._crop_canvas.width, this._crop_canvas.height);
+  }
 
-    const self = this;
+  startToCut() {
+    this.image = new Image();
+    this.image.setAttribute('crossOrigin', 'anonymous');
+    this.image.src = this.options.source;
+    const vm = this;
 
-    let imageWidth;
-    if (imageStyle && imageStyle.width) {
-      imageWidth = imageStyle.width;
-    }
-
-    let vm = this;
-
-    const imagesPanel = imagesArr.map(function (image, index) {
-
-      let width, height;
-
-      let style = {
-        opacity: '0',
-        transition: 'opacity 0.8s'
+    this.image.onload = function() {
+      console.log('this.imageWidth:', vm.image.width);
+      vm.initCanvas('myCanvas', vm.image.width, vm.image.height);
+      switch (vm.type) {
+        case 'ellipse':
+          return vm.cutToEllipe();
+        case 'rect':
+          return vm.cutToRectTangle();
+        case 'circle':
+          return vm.cutToCirCle();
+        default:
+          return '';
       }
-
-      let imageUrl = vm.getSrc(image);
-
-      if (imageWidth) {
-
-        const search = imageUrl.substr(imageUrl.indexOf('?'));
-
-        if (!width) {
-          width = getQueryName('w', search);
-        }
-        if (!height) {
-          height = getQueryName('h', search);
-        }
-
-        if (width && height) {
-          let imageHeight = height * 1.0 / width * imageWidth;
-          let remHeight = resize(imageHeight);
-          style.minHeight = remHeight + 'rem';
-        }
-      }
-
-      return (
-        <img
-          key={image + index}
-          ref={ ref => { self.registImageNode(ref, index) } }
-          src={ imageUrl }
-          onLoad={self.onLoadImage.bind(vm, index)}
-          onError={self.onImageError.bind(vm, index)}
-          style={style}
-        />
-      )
-    });
-
-    return imagesPanel;
+    };
   }
 }
 
-export default ImageLoaderV2;
+
+export default PictureCut;
 
